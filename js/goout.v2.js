b345d0ddc2d33851df8818a88c911d54e0ef7b2e
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-17 03:59:37 +0900

    Update goout.v2.js

--- js/goout.v2.js
+++ js/goout.v2.js
@@ -151,6 +151,7 @@ function ChangeHidden() {
       main.hidden = true;
       enter.innerText = "Let's have some fun";
       map.flyTo({
+        center: center,
         zoom: 1.75,
         scrollZoom: false
       });
