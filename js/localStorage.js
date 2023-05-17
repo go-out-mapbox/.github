ae9c9fd2003eb95b2d04a6488e7f74dcebdf0b10
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-17 18:21:53 +0900

    Update localStorage.js

--- js/localStorage.js
+++ js/localStorage.js
@@ -17,23 +17,23 @@ for (let i = 0; i < mapJSON.length; i++) {
       'date': '${thisDate}',
       'on': '${thisOn}',
   },`
-}
 
-const stores = {
-  'type': 'FeatureCollection',
-  'features': [features
-  {
-    'type': 'Feature',
-    'geometry': {
-      'type': 'Point',
-      'coordinates': [135.4854249,34.616754]
-    },
-    'properties': {
-      'title': '135.4854249,34.616754',
-      'address': '日本, 大阪府大阪市住之江区粉浜西3丁目1番8',
-      'date': 'Under Construction',
-      'on': 'Wed May 17 2023 17:30:30 GMT+0900 (日本標準時)',
+  const stores = {
+    'type': 'FeatureCollection',
+    'features': [features
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [135.4854249,34.616754]
+      },
+      'properties': {
+        'title': '135.4854249,34.616754',
+        'address': '日本, 大阪府大阪市住之江区粉浜西3丁目1番8',
+        'date': 'Under Construction',
+        'on': 'Wed May 17 2023 17:30:30 GMT+0900 (日本標準時)',
+      }
     }
-  }
-]
-};
+  ]
+  };
+}
