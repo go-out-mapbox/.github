27ee30d60db16695ed9d3d1cea2475bfd398d9a7
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-18 19:50:11 +0900

    Update localStorage.js

--- js/localStorage.js
+++ js/localStorage.js
@@ -25,65 +25,68 @@ if(localStorage.getItem("map")) {
   const mapJSON = JSON.parse(localStorage.getItem('map'));
   mapJSON.forEach((marker, i) => {
     let coordinates = marker.title;
-    let thisTitle = marker.address;
+    let thisAddress = marker.address;
     let thisDate = marker.date;
     let thisOn = marker.timestamp;
-    let yourMarker = `{
+    let yourMarker = `
+    {
       'type': 'Feature',
       'geometry': {
         'type': 'Point',
         'coordinates': [${coordinates}]
       },
       'properties': {
-        'title': ${coordinates},
-        'address': ${thisTitle},
-        'date': ${thisOn},
+        'title': '${coordinates}',
+        'address': '${thisAddress}',
+        'date': '${thisDate}',
+        'timestamp': '${thisOn},',
         'tags': 'marker',
-      }`
-    });
-
-    stores.features.push(`${yourMarker}`)
-  }
-
-  // 地図にマーカーを追加
-  map.on('load', () => {
-    map.addSource('places', {
-      'type': 'geojson',
-      'data': stores
-    });
-    addMarkers();
+      }
+    }`
   });
 
-  function addMarkers() {
-    for (const marker of stores.features) {
-      const el = document.createElement('div');
-      el.id = `marker-${marker.properties.id}`;
-      el.className = marker.properties.tags;
-      new mapboxgl.Marker(el, {
-        offset: [0, 0]
-      })
-      .setLngLat(marker.geometry.coordinates)
-      .addTo(map);
+  stores.features.push(`${yourMarker}`)
+}
 
-      el.addEventListener('click', (e) => {
-        flyToStore(marker);
-        createPopUp(marker);
-      });
-    }
+// 地図にマーカーを追加
+map.on('load', () => {
+  map.addSource('places', {
+    'type': 'geojson',
+    'data': stores
+  });
+  addMarkers();
+});
+
+function addMarkers() {
+  for (const marker of stores.features) {
+    const el = document.createElement('div');
+    el.id = `marker-${marker.properties.id}`;
+    el.className = marker.properties.tags;
+    new mapboxgl.Marker(el, {
+      offset: [0, 0]
+    })
+    .setLngLat(marker.geometry.coordinates)
+    .addTo(map);
+
+    el.addEventListener('click', (e) => {
+      flyToStore(marker);
+      createPopUp(marker);
+    });
   }
+}
 
-  /* Create a Mapbox GL JS `Popup`. */
-  function createPopUp(currentFeature) {
-    dateSection.innerHTML = "";
-    yourHere.textContent = currentFeature.properties.title;
-    yourAddress.textContent = currentFeature.properties.address;
-    dateSection.className = currentFeature.properties.tags;
+/* Create a Mapbox GL JS `Popup`. */
+function createPopUp(currentFeature) {
+  dateSection.innerHTML = "";
+  yourHere.textContent = currentFeature.properties.title;
+  yourAddress.textContent = currentFeature.properties.address;
+  dateSection.className = currentFeature.properties.tags;
 
-    const dateTXT = document.createElement('p');
-    dateTXT.innerHTML = currentFeature.properties.date;
-    dateSection.appendChild(dateTXT);
+  const dateTXT = document.createElement('p');
+  dateTXT.innerHTML = currentFeature.properties.date;
+  dateSection.appendChild(dateTXT);
 
-    const dateTime = document.createElement('p');
-    dateTime.innerHTML = `<time>${currentFeature.properties.timestamp}</time>`;
-    dateSection.appendChild(dateTime);
-  }
+  const dateTime = document.createElement('p');
+  dateTime.innerHTML = `<time>${currentFeature.properties.timestamp}</time>`;
+  dateSection.appendChild(dateTime);
+}
