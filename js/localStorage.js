5b3d03bab9d6bf1933723dbcf64209df80ca8704
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-18 14:25:40 +0900

    Update localStorage.js

--- js/localStorage.js
+++ js/localStorage.js
@@ -6,28 +6,27 @@ map.on('load', () => {
       'type': 'FeatureCollection'
     }
   });
-  addMarkers();
-});
 
-function addMarkers() {
-  let mapJSON = JSON.parse(localStorage.getItem('map'));
-  for (let i = 0; i < mapJSON.length; i++) {
-    let coordinates = mapJSON[i].geolocation
-    let thisTitle = mapJSON[i].address
-    let thisDate = mapJSON[i].date
-    let thisOn = mapJSON[i].timestamp
+  if(localStorage.getItem('map')) {
+    let mapJSON = JSON.parse(localStorage.getItem('map'));
+    for (let i = 0; i < mapJSON.length; i++) {
+      let coordinates = mapJSON[i].geolocation
+      let thisTitle = mapJSON[i].address
+      let thisDate = mapJSON[i].date
+      let thisOn = mapJSON[i].timestamp
 
-    const el = document.createElement('div');
-    el.id = `marker-${i}`;
-    el.className = 'marker';
-    new mapboxgl.Marker(el, {
-      offset: [0, -23]
-    })
-    .setLngLat(coordinates)
-    .addTo(map);
+      const el = document.createElement('div');
+      el.id = `marker-${i}`;
+      el.className = 'marker';
+      new mapboxgl.Marker(el, {
+        offset: [0, -23]
+      })
+      .setLngLat(coordinates)
+      .addTo(map);
 
-    el.addEventListener('click', (e) => {
-      flyToStore(coordinates);
-    });
+      el.addEventListener('click', (e) => {
+        flyToStore(coordinates);
+      });
+    }
   }
-}
+});
