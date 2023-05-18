7d00965769908540dbe31c9e20a601ddc93afffe
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-18 14:33:09 +0900

    Update localStorage.js

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,32 +1,41 @@
 // 地図にマーカーを追加
+async function indexJSON() {
+  const requestURL = 'index.json';
+  const request = new Request(requestURL);
+  const response = await fetch(request);
+  const jsonIndex = await response.text();
+
+  const index = JSON.parse(jsonIndex);
+}
+
 map.on('load', () => {
   map.addSource('places', {
     'type': 'geojson',
     'data': {
-      'type': 'FeatureCollection'
+      'type': 'FeatureCollection',
+      'features' : addMarkers();
     }
   });
-
-  if(localStorage.getItem('map')) {
-    let mapJSON = JSON.parse(localStorage.getItem('map'));
-    for (let i = 0; i < mapJSON.length; i++) {
-      let coordinates = mapJSON[i].geolocation
-      let thisTitle = mapJSON[i].address
-      let thisDate = mapJSON[i].date
-      let thisOn = mapJSON[i].timestamp
-
-      const el = document.createElement('div');
-      el.id = `marker-${i}`;
-      el.className = 'marker';
-      new mapboxgl.Marker(el, {
-        offset: [0, -23]
-      })
-      .setLngLat(coordinates)
-      .addTo(map);
-
-      el.addEventListener('click', (e) => {
-        flyToStore(coordinates);
-      });
-    }
-  }
 });
+function addMarkers() {
+  let mapJSON = JSON.parse(localStorage.getItem('map'));
+  for (let i = 0; i < mapJSON.length; i++) {
+    let coordinates = mapJSON[i].geolocation
+    let thisTitle = mapJSON[i].address
+    let thisDate = mapJSON[i].date
+    let thisOn = mapJSON[i].timestamp
+
+    const el = document.createElement('div');
+    el.id = `marker-${i}`;
+    el.className = 'marker';
+    new mapboxgl.Marker(el, {
+      offset: [0, -23]
+    })
+    .setLngLat(coordinates)
+    .addTo(map);
+
+    el.addEventListener('click', (e) => {
+      flyToStore(coordinates);
+    });
+  }
+}
