217d193a4171f38b16a94b1711f5291133ec620d
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-18 15:18:19 +0900

    Update localStorage.js

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,12 +1,4 @@
-// 地図にマーカーを追加
-async function indexJSON() {
-  const requestURL = 'index.json';
-  const request = new Request(requestURL);
-  const response = await fetch(request);
-  const jsonIndex = await response.text();
-
-  const index = JSON.parse(jsonIndex);
-}
+'use strict'
 
 map.on('load', () => {
   map.addSource('places', {
@@ -15,32 +7,42 @@ map.on('load', () => {
       'type': 'FeatureCollection'
     }
   })
-
-  if(localStorage.getItem('map')) {
-    let mapJSON = JSON.parse(localStorage.getItem('map'));
-
-    mapJSON.forEach((markerID, i) => {
-      markerID.id = i;
-    });
-
-    for (const marker of mapJSON) {
-      let coordinates = marker.geolocation;
-      let thisTitle = marker.address;
-      let thisDate = marker.date;
-      let thisOn = marker.timestamp;
-
-      const el = document.createElement('div');
-      el.id = `marker-${markerID.id}`;
-      el.className = 'marker';
-      new mapboxgl.Marker(el, {
-        offset: [0, -23]
-      });
-      .setLngLat(coordinates);
-      .addTo(map);
-
-      el.addEventListener('click', (e) => {
-        flyToStore(marker);
-      });
-    }
-  }
 });
+
+if(localStorage.getItem('map')) {
+  const mapJSON = JSON.parse(localStorage.getItem('map'));
+  mapJSON.forEach((marker, i) => {
+    let coordinates = marker.geolocation;
+    let thisTitle = marker.address;
+    let thisDate = marker.date;
+    let thisOn = marker.timestamp;
+
+    const el = document.createElement('div');
+    el.id = `marker-${i}`;
+    el.className = 'marker';
+    el.innerText = coordinates;
+    dateSection.prepend(el)
+  });
+}
+
+
+
+for (const marker of mapJSON) {
+  let coordinates = marker.geolocation;
+  let thisTitle = marker.address;
+  let thisDate = marker.date;
+  let thisOn = marker.timestamp;
+
+  const el = document.createElement('div');
+  el.id = `marker-${markerID.id}`;
+  el.className = 'marker';
+  new mapboxgl.Marker(el, {
+    offset: [0, -23]
+  });
+  .setLngLat(coordinates);
+  .addTo(map);
+
+  el.addEventListener('click', (e) => {
+    flyToStore(marker);
+  });
+}
