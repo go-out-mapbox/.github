59555738df278eb1d0f11c2e7750a2cf44912bcc
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-18 15:19:20 +0900

    Update localStorage.js

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,14 +1,5 @@
 'use strict'
 
-map.on('load', () => {
-  map.addSource('places', {
-    'type': 'geojson',
-    'data': {
-      'type': 'FeatureCollection'
-    }
-  })
-});
-
 if(localStorage.getItem('map')) {
   const mapJSON = JSON.parse(localStorage.getItem('map'));
   mapJSON.forEach((marker, i) => {
@@ -22,27 +13,5 @@ if(localStorage.getItem('map')) {
     el.className = 'marker';
     el.innerText = coordinates;
     dateSection.prepend(el)
-  });
-}
-
-
-
-for (const marker of mapJSON) {
-  let coordinates = marker.geolocation;
-  let thisTitle = marker.address;
-  let thisDate = marker.date;
-  let thisOn = marker.timestamp;
-
-  const el = document.createElement('div');
-  el.id = `marker-${markerID.id}`;
-  el.className = 'marker';
-  new mapboxgl.Marker(el, {
-    offset: [0, -23]
-  });
-  .setLngLat(coordinates);
-  .addTo(map);
-
-  el.addEventListener('click', (e) => {
-    flyToStore(marker);
   });
 }
