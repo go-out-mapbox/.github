0ce3c099cca4e16858fc92658e447aa75070f6d2
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-21 16:03:05 +0900

    map

---
+++ submit/json.php
@@ -0,0 +1,21 @@
+<?php
+
+$source_file = "submit/stores.json";
+$ip = $_SERVER["REMOTE_ADDR"];
+
+define("LOGFILE", $source_file);
+$data = json_decode(file_get_contents("php://input"), true);
+
+$output = array(
+  $data["longitude"],
+  $data["latitude"],
+  '"'. $data["address"] .'"',
+  '"'. $data["date"] .'"',
+  '"'. $data["timestamp"] .'"',
+  '"'. $ip .'"'
+);
+
+$result = implode(',', $output);
+file_put_contents(LOGFILE, $result."\n", FILE_APPEND | LOCK_EX);
+echo json_encode($output);
+?>
---
+++ submit/script.js
@@ -0,0 +1,66 @@
+'use strict'
+
+// localStorage に 投稿 を追加
+let array = JSON.parse(localStorage.getItem("map")) || [];
+const addData = (longitude, latitude, address, date, timestamp) => {
+  array.push({
+    longitude,
+    latitude,
+    address,
+    date,
+    timestamp
+  })
+
+  localStorage.setItem("map", JSON.stringify(array))
+  return {longitude, latitude, address, date, timestamp}
+}
+
+
+// 現在地を投稿する
+submitForm.addEventListener('submit', submitThis)
+async function submitThis() {
+  event.preventDefault()
+
+  // localStorage に 投稿 を追加する
+  const thisLongitude = document.querySelector('#date #longitude').textContent
+  const thisLatitude = document.querySelector('#date #latitude').textContent
+  const thisAddress = yourAddress.textContent
+
+  const thisTXT = document.querySelector('#submit #comment');
+  let text = thisTXT.value
+  let textArray = text.split('\n')
+  let thisDate = textArray.join('<br>')
+  const thisTime = document.querySelector('#date section time').textContent
+  addData(thisLongitude, thisLatitude, thisAddress, thisDate, thisTime)
+
+  // 投稿を JSON形式で PHPに送信
+  let thisPin = {
+    longitude : thisLongitude,
+    latitude : thisLatitude,
+    address : thisAddress,
+    date : thisDate,
+    timestamp : thisTime
+  };
+
+  const thisJSON = JSON.stringify(thisPin)
+  let url = 'submit/json.php'
+  let response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: thisJSON
+  })
+
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+  setTimeout(() => {
+    window.location.replace('/map/profile/')
+  }, 1000)
+}
