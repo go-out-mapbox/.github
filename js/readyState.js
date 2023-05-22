77597069af6404799d510ae7296f09801cd40722
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-22 15:05:58 +0900

    Update readyState.js

--- js/readyState.js
+++ js/readyState.js
@@ -143,34 +143,6 @@ document.addEventListener('readystatechange', event => {
         zoom: 11.11
       })
     })
-
-    if(localStorage.getItem("map")) {
-      const mapJSON = JSON.parse(localStorage.getItem('map'))
-      for (let i = 0; i < mapJSON.length; i++) {
-        let thisLongitude = mapJSON[i].longitude,
-        thisLatitude = mapJSON[i].latitude,
-        thisAddress = mapJSON[i].address,
-        thisDate = mapJSON[i].date,
-        thisOn = mapJSON[i].timestamp;
-
-        let thisCenter = [thisLongitude, thisLatitude]
-        let yourMarker = {
-          'type': 'Feature',
-          'geometry': {
-            'type': 'Point',
-            'coordinates': thisCenter
-          },
-          'properties': {
-            'title': `${thisLongitude}, ${thisLatitude}`,
-            'address': thisAddress,
-            'date': thisDate,
-            'timestamp': thisOn,
-            'tags': 'marker',
-          }
-        }
-        stores.features.push(yourMarker)
-      }
-    }
   } else if (event.target.readyState === 'complete') {
     // 地図にマーカーを追加
     map.on('load', () => {
