2dcc0d78d092c569b31c8ecfa828a7dcecd9199e
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-22 14:16:34 +0900

    map

--- localStorage.js
+++ localStorage.js
@@ -2,20 +2,23 @@
 
 // localStorage から 投稿 を取得
 if(localStorage.getItem("map")) {
-  const mapJSON = JSON.parse(localStorage.getItem('map'));
+  const mapJSON = JSON.parse(localStorage.getItem('map'))
   for (let i = 0; i < mapJSON.length; i++) {
-    let coordinates = mapJSON[i].title;
-    let thisAddress = mapJSON[i].address;
-    let thisDate = mapJSON[i].date;
-    let thisOn = mapJSON[i].timestamp;
+    let thisLongitude = mapJSON[i].longitude,
+    thisLatitude = mapJSON[i].latitude,
+    thisAddress = mapJSON[i].address,
+    thisDate = mapJSON[i].date,
+    thisOn = mapJSON[i].timestamp;
+
+    let thisCenter = [thisLongitude, thisLatitude];
     let yourMarker = {
       'type': 'Feature',
       'geometry': {
         'type': 'Point',
-        'coordinates': [coordinates]
+        'coordinates': thisCenter
       },
       'properties': {
-        'title': coordinates,
+        'title': `${thisLongitude}, ${thisLatitude}`,
         'address': thisAddress,
         'date': thisDate,
         'timestamp': thisOn,
@@ -23,5 +26,5 @@ if(localStorage.getItem("map")) {
       }
     }
     stores.features.push(yourMarker)
-  };
+  }
 }
