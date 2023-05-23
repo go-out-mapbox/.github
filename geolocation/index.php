9eeaeed4417d5a418944e6c4bf45eb2dc5ab495d
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-23 21:23:06 +0900

    Update index.php

--- geolocation/index.php
+++ geolocation/index.php
@@ -53,6 +53,27 @@
   // ページにMapboxを埋め込む
   mapboxgl.accessToken = 'pk.eyJ1IjoicGVodSIsImEiOiJja3R4Y3diNmIybTg5Mm9waWgwYTdsc3FyIn0.lVvnPZ3aa6332EaWJIxPaQ';
   let center = [0, 0];
+
+  // 現在位置を取得できた場合の処理
+  function success(position) {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    let center = [longitude, latitude];
+  };
+
+  // 現在位置を取得できなかった場合の処理
+  function error() {
+    let center = [135.50433479522678, 34.69699057458179];
+  };
+
+  // 現在位置を取得する
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  map.flyTo({
+    center: center,
+    zoom: 15
+  });
+
   const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/pehu/ckx1e2xhw13kw14s4rjhaiv17',
