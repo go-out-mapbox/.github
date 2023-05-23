1fce5da6c72fa3186049db72ca5d041c26f21456
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-24 02:57:52 +0900

    map

--- geolocation/readyState.js
+++ geolocation/readyState.js
@@ -1,30 +1,5 @@
 'use strict'
 
-function storageAvailable(type) {
-  var storage;
-  try {
-    storage = window[type];
-    var x = '__storage_test__';
-    storage.setItem(x, x);
-    storage.removeItem(x);
-    return true;
-  } catch(e) {
-    return e instanceof DOMException && (
-      e.code === 22 ||
-      e.code === 1014 ||
-      e.name === 'QuotaExceededError' ||
-      e.name === 'NS_ERROR_DOM_QUOTA_REACHED'
-    ) && (storage && storage.length !== 0);
-  }
-}
-
-if (!storageAvailable('localStorage')) {
-  window.location.replace('/map/')
-}
-if (!'geolocation' in navigator) {
-  window.location.replace('/map/')
-}
-
 document.addEventListener('readystatechange', event => {
   if (event.target.readyState === 'interactive') {
     const title = document.querySelector('h1');
@@ -45,7 +20,7 @@ document.addEventListener('readystatechange', event => {
         center: center,
         bearing: 0,
         pitch: 60,
-        zoom: 15
+        zoom: 17.5
       });
 
       let marker = new mapboxgl.Marker({
