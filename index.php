9057d820e8301c771622c031e37832c8fc0823ed
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-24 01:26:45 +0900

    Update index.php

--- index.php
+++ index.php
@@ -79,19 +79,6 @@
     scrollZoom: true
   });
 
-  // 現在位置を取得するボタンを追加
-  map.addControl(
-    new mapboxgl.GeolocateControl({
-      positionOptions: {
-        enableHighAccuracy: true
-      },
-      // デバイスの位置の変更に応じて位置情報を更新
-      trackUserLocation: true,
-      // デバイスが向いている方向を矢印で描画
-      showUserHeading: true
-    })
-  );
-
   map.addControl(new mapboxgl.NavigationControl());
   </script>
   <script src="js/markers.js"></script>
