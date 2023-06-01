c9a4df24d476912472bea55a286e09be5200a81e
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-01 22:11:47 +0900

    Update index.php

--- index.php
+++ index.php
@@ -88,7 +88,6 @@
   </script>
   <script src="js/markers.v2.js"></script>
   <script src="js/polygon.v3.js"></script>
-  <script src="getCSV.js"></script>
   <script src="js/readyState.v3.js"></script>
   <script src="js/submit.js"></script>
   <script src="/js/log.js"></script>
