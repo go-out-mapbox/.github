7872177faaa6ba7d1371bda20721ab4ae7a52514
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-13 22:57:24 +0900

    map

--- js/markers.v2.js
+++ js/markers.v2.js
@@ -1,3 +1,5 @@
+"use strict"
+
 let stores = {
   'type': 'FeatureCollection',
   'features': [
@@ -59,3 +61,40 @@ let stores = {
     }
   ]
 }
+
+
+async function loadCSV(targetCSV) {
+  const request = new Request(targetCSV);
+  const response = await fetch(request);
+  const csv = await response.text();
+
+  // CSVの全行を取得
+  const lines = csv.split("\n");
+  for (let i = 0; i < lines.length; i++) {
+    // 1行ごとの処理
+    let cells = lines[i].split(",");
+
+    for( let ii = 0; ii < cells.length; ii++ ) {
+      var removDouble = cells[ii].replace(/\"/g,"");
+      cells[ii] = removDouble;
+    }
+
+    let addMarker = {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [cells[0], cells[1]]
+      },
+      'properties': {
+        'title': [cells[0], cells[1]],
+        'address': cells[2],
+        'date': cells[3],
+        'timestamp': cells[4],
+        'tags': 'marker',
+      }
+    }
+    stores.features.push(addMarker)
+  }
+}
+
+loadCSV("date/2023.csv");
