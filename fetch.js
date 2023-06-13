7ed83b36767fbef84b9c4a96444e745add72e35f
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-13 23:49:02 +0900

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -5,10 +5,10 @@ loadCSV("date/2023.csv");
 async function loadCSV(targetCSV) {
   const request = new Request(targetCSV);
   const response = await fetch(request);
-  const csv = await response.text();
+  const dateCSV = await response.text();
 
   // CSVの全行を取得
-  const lines = csv.split("\n");
+  const lines = dateCSV.split("\n");
   for (let i = 0; i < lines.length; i++) {
     // 1行ごとの処理
     let cells = lines[i].split(",");
@@ -23,7 +23,7 @@ async function loadCSV(targetCSV) {
       'type': 'Feature',
       'geometry': {
         'type': 'Point',
-        'coordinates': addCenter
+        'coordinates': [cells[0], cells[1]]
       },
       'properties': {
         'title': addCenter,
