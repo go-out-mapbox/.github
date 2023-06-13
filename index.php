dd99aa29bd1b492389430a824680f990e7669edf
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-13 22:06:27 +0900

    map

--- index.php
+++ index.php
@@ -65,7 +65,7 @@
   <article id="map"></article>
 
   <script src="js/markers.v2.js"></script>
-  <script src="XMLHttpRequest.js"></script>
+  <script src="fetch.js"></script>
 
   <script type="text/javascript">
   const mapbox = document.querySelector('#map'),
