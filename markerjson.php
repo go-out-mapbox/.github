86e8c144c85d74aaf6d5d7d6337ffabafb41e557
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-14 00:31:07 +0900

    json

--- markerjson.php
+++ markerjson.php
@@ -3,7 +3,7 @@ mb_language("ja");
 mb_internal_encoding("UTF-8");
 date_default_timezone_set('Asia/Tokyo');
 
-$source_file = "date/". date("Y") .".js";
+$source_file = "date/". date("Y") .".json";
 $ip = $_SERVER["REMOTE_ADDR"];
 
 define("LOGFILE", $source_file);
@@ -11,7 +11,7 @@ $data = json_decode(file_get_contents("php://input"), true);
 
 // JSONファイルの確認
 if(!file_exists(LOGFILE)) {
-  file_put_contents(LOGFILE, "'features': []");
+  file_put_contents(LOGFILE, "[]");
 }
 
 $json = file_get_contents(LOGFILE);
