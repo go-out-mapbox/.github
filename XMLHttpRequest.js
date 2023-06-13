ace396b1c89a66fffd46d6ee90848497dd26987f
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-13 22:05:16 +0900

    Update XMLHttpRequest.js

--- XMLHttpRequest.js
+++ XMLHttpRequest.js
@@ -18,7 +18,7 @@ async function loadCSV(targetCSV) {
       cells[ii] = removDouble;
     }
 
-    let heardCenter = cells[0], cells[1];
+    let heardCenter = [cells[0], cells[1]];
     let addMarker = {
       'type': 'Feature',
       'geometry': {
